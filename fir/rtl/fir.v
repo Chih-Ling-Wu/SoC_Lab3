7200fa153f2983df0b479ae8c1797e98780467d9
Doggoro <99811661+Chih-Ling-Wu@users.noreply.github.com>
2023-09-29 22:34:50 +0800

    Update fir.v

--- fir/rtl/fir.v
+++ fir/rtl/fir.v
@@ -1,154 +1,215 @@
-module fir
-#(
-    parameter pADDR_WIDTH = 12,
-    parameter pDATA_WIDTH = 32,
-    parameter Tape_Num    = 11
-)
-(
-    output  wire                     awready,
-    output  wire                     wready,
-    input   wire                     awvalid,
-    input   wire [(pADDR_WIDTH-1):0] awaddr,
-    input   wire                     wvalid,
-    input   wire [(pDATA_WIDTH-1):0] wdata,
-    output  wire                     arready,
-    input   wire                     rready,
-    input   wire                     arvalid,
-    input   wire [(pADDR_WIDTH-1):0] araddr,
-    output  wire                     rvalid,
-    output  wire [(pDATA_WIDTH-1):0] rdata,    
-    input   wire                     ss_tvalid, 
-    input   wire [(pDATA_WIDTH-1):0] ss_tdata, 
-    input   wire                     ss_tlast, 
-    output  wire                     ss_tready, 
-    input   wire                     sm_tready, 
-    output  wire                     sm_tvalid, 
-    output  wire [(pDATA_WIDTH-1):0] sm_tdata, 
-    output  wire                     sm_tlast, 
-    input   wire                     axis_clk,
-    input   wire                     axis_rst_n
+module fir_filter (
+    // AXI-Lite Control Interface
+    output wire awready,
+    output wire wready,
+    input wire awvalid,
+    input wire [11:0] awaddr,
+    input wire wvalid,
+    input wire [31:0] wdata,
+    output wire arready,
+    input wire rready,
+    input wire arvalid,
+    input wire [11:0] araddr,
+    output wire rvalid,
+    output wire [31:0] rdata,
+
+    // AXI-Stream Input Interface
+    input wire ss_tvalid,
+    input wire [31:0] ss_tdata,
+    input wire ss_tlast,
+    output wire ss_tready,
+
+    // AXI-Stream Output Interface
+    input wire sm_tready,
+    output wire sm_tvalid,
+    output wire [31:0] sm_tdata,
+    output wire sm_tlast,
+
+    // Clock and Reset
+    input wire axis_clk,
+    input wire axis_rst_n
 );
-    // Internal signals and registers
-    reg [(pDATA_WIDTH-1):0] shift_reg [0:10]; // Size = 11 DW
-    reg [(pDATA_WIDTH-1):0] output_data;
-    reg [pADDR_WIDTH-1:0] tap_data_addr;
-    reg [pDATA_WIDTH-1:0] tap_data;
-    reg [pDATA_WIDTH-1:0] len;
-    reg ap_start;
-    reg ap_done;
-    reg ap_idle;
+    // Define internal signals and registers
+    reg [31:0] shift_reg [10:0]; // Shift register implemented with SRAM (11 DW)
+    reg [31:0] tap_coeff [10:0]; // Tap coefficients implemented with SRAM (11 DW)
+    reg [31:0] accum;
+    reg [31:0] output_data;
+    reg [4:0] tap_ptr;
+    reg [31:0] data_count;
+    wire tap_wr_enable;
+    wire ap_start;
 
-    // FIR filter coefficients
-    reg signed [(pDATA_WIDTH-1):0] tap_coefficients [0:10];
+    // Internal AXI-Lite Control Registers
+    reg [31:0] coef_write_data;
+    reg coef_write_enable;
+    reg [3:0] coef_write_addr;
+    reg coef_write_done;
+    reg [31:0] len_write_data;
+    reg len_write_enable;
+    reg [31:0] len_read_data;
+    reg [31:0] ap_start_read_data;
+    reg ap_start_write_enable;
+    reg [31:0] ap_done_write_data;
+    reg ap_done_write_enable;
 
-    // Additional logic for sm_tvalid, sm_tdata, sm_tlast, and sm_tready
-    reg sm_tvalid;   // Valid signal for the output data
-    reg [(pDATA_WIDTH-1):0] sm_tdata; // Output data
-    reg sm_tlast;    // Last signal for the output data
-    wire sm_tready;  // Ready signal for the output data
+    // Address map constants
+    localparam ADDR_AP_CTRL = 12'h00;
+    localparam ADDR_DATA_LENGTH = 12'h10;
+    localparam ADDR_TAP_PARAMS_START = 12'h20;
+    localparam ADDR_TAP_PARAMS_END = 12'hFF;
 
-    // Define some internal signals to manage the streaming output
-    reg [2:0] output_state; // State machine for output streaming
+    // Initialization
+    initial begin
+        // Initialize your shift register, tap coefficients, and control signals here.
+        // For example:
+        for (i = 0; i < 11; i = i + 1) begin
+            shift_reg[i] = 0;
+            tap_coeff[i] = 0;
+        end
+        accum = 0;
+        output_data = 0;
+        tap_ptr = 0;
+        data_count = 0;
+        coef_write_data = 0;
+        coef_write_enable = 0;
+        coef_write_addr = 0;
+        coef_write_done = 0;
+        ap_start_write_enable = 0;
+        ap_done_write_data = 0;
+        len_write_data = 0;
+        len_write_enable = 0;
+        len_read_data = 0;
+        ap_start_read_data = 0;
+        ap_start = 0;
+    end
 
+    // AXI-Lite Control Logic
     always @(posedge axis_clk or negedge axis_rst_n) begin
         if (!axis_rst_n) begin
-            // Reset logic for the output streaming
-            output_state <= 3'b000;
-            sm_tvalid <= 0;
-            sm_tdata <= 0;
-            sm_tlast <= 0;
+            // Reset control signals
         end else begin
-            // Handle the output streaming
-            case(output_state)
-                3'b000: begin
-                    // Idle state
-                    if (ss_tvalid) begin
-                        output_state <= 3'b001; // Move to the first data state
-                        sm_tvalid <= 1; // Indicate that data is valid
-                        sm_tdata <= ss_tdata; // Output the data
-                        sm_tlast <= ss_tlast; // Output the last signal
+            // AXI-Lite address decoding
+            case(awaddr)
+                ADDR_AP_CTRL: begin
+                    if (awvalid && !ap_start) begin
+                        ap_start <= 1;
+                        ap_done_write_enable <= 0;
                     end
                 end
-
-                3'b001: begin
-                    // Data state
-                    if (sm_tready) begin
-                        output_state <= 3'b000; // Go back to idle state
-                        sm_tvalid <= 0; // Indicate that data is not valid
+                ADDR_DATA_LENGTH: begin
+                    if (awvalid) begin
+                        len_write_data <= wdata;
+                        len_write_enable <= 1;
                     end
+                end
+                ADDR_TAP_PARAMS_START: begin
+                    if (awvalid) begin
+                        coef_write_data <= wdata;
+                        coef_write_enable <= 1;
+                        coef_write_addr <= awaddr[7:2];
+                    end
+                end
+                default: begin
+                    coef_write_enable <= 0;
+                    len_write_enable <= 0;
                 end
             endcase
         end
     end
 
-    // FIR filter logic
+    // Shift register implementation using SRAM
+    always @(posedge axis_clk or posedge axis_rst_n) begin
+        if (!axis_rst_n) begin
+            // Reset shift register
+            for (i = 0; i < 11; i = i + 1) begin
+                shift_reg[i] = 0;
+            end
+        end else if (ap_start) begin
+            // Reset shift register on ap_start
+            for (i = 0; i < 11; i = i + 1) begin
+                shift_reg[i] = 0;
+            end
+        end else if (ss_tvalid) begin
+            // Shift in data when valid
+            for (i = 10; i > 0; i = i - 1) begin
+                shift_reg[i] <= shift_reg[i - 1];
+            end
+            shift_reg[0] <= ss_tdata;
+        end
+    end
+
+    // FIR filtering logic
+    always @(posedge axis_clk or posedge axis_rst_n) begin
+        if (!axis_rst_n) begin
+            // Reset FIR filter
+            accum <= 0;
+            output_data <= 0;
+        end else if (ap_start) begin
+            // Reset FIR filter on ap_start
+            accum <= 0;
+            output_data <= 0;
+        end else if (ss_tvalid) begin
+            // Compute FIR filter output
+            if (tap_wr_enable) begin
+                accum <= 0;
+            end else begin
+                accum <= accum + shift_reg[tap_ptr] * tap_coeff[tap_ptr];
+                output_data <= accum;
+            end
+        end else if (ss_tready) begin
+            accum <= accum - shift_reg[tap_ptr] * tap_coeff[tap_ptr];
+            output_data <= accum;
+        end
+    end
+
+    // Output data to AXI-Stream interface
     always @(posedge axis_clk or negedge axis_rst_n) begin
         if (!axis_rst_n) begin
-            // Reset logic here (initialize registers, etc.)
-            for (int i = 0; i < 11; i = i + 1) begin
-                shift_reg[i] <= 0;
-                tap_coefficients[i] <= 0;
-            end
-            len <= 0;
-            ap_start <= 0;
-            ap_done <= 0;
-            ap_idle <= 0;
+            // Reset AXI-Stream signals
         end else begin
-            // Handle data input (axi_stream to shift register)
-            if (!ap_start && awvalid && awready) begin
-                ap_start <= awdata[0];
-            end
+            // Output data to AXI-Stream
+            sm_tvalid <= (data_count < len_read_data);
+            sm_tdata <= output_data;
+            sm_tlast <= (data_count == len_read_data - 1);
+        end
+    end
 
-            if (ap_start) begin
-                if (awvalid && awready) begin
-                    shift_reg[0] <= wdata;
-                    for (int i = 1; i < 11; i = i + 1) begin
-                        shift_reg[i] <= shift_reg[i - 1];
-                    end
+    // AXI-Stream input interface logic
+    always @(posedge axis_clk or negedge axis_rst_n) begin
+        if (!axis_rst_n) begin
+            // Reset AXI-Stream input signals
+            ss_tready <= 0;
+        end else if (ap_start) begin
+            // Reset AXI-Stream input on ap_start
+            ss_tready <= 0;
+        end else begin
+            // AXI-Stream input logic
+            ss_tready <= sm_tready;
+            if (ss_tvalid && ss_tready) begin
+                data_count <= data_count + 1;
+                if (data_count == len_read_data - 1) begin
+                    ap_done_write_data <= 1;
                 end
-
-                // Calculate FIR output using multiplications and accumulation
-                output_data <= 0;
-                for (int i = 0; i < 11; i = i + 1) begin
-                    output_data <= output_data + (shift_reg[i] * tap_coefficients[i]);
-                end
-
-                // Handle data output (axi_stream)
-                if (sm_tvalid && sm_tready) begin
-                    ss_tready <= 0;
-                    ss_tdata <= output_data;
-                    ss_tlast <= sm_tlast;
-                end
-
-                // Check for ap_done (when all data is processed)
-                if (ss_tvalid && ss_tlast) begin
-                    ap_done <= 1;
-                end
-            end
-
-            // Handle axilite registers
-            if (arvalid && arready) begin
-                case(araddr)
-                    12'h20: rdata <= tap_coefficients[araddr[7:0]];
-                    12'h10: rdata <= len;
-                    12'h00: rdata <= {ap_idle, ap_done, ap_start};
-                    // Add more cases for other registers if needed
-                    default: rdata <= 32'h0;
-                endcase
-                rvalid <= 1;
-            end else begin
-                rvalid <= 0;
-            end
-
-            // Handle axilite writes
-            if (ap_start && awvalid && awready) begin
-                case(awaddr)
-                    12'h20: tap_coefficients[awaddr[7:0]] <= wdata;
-                    12'h10: len <= wdata;
-                    12'h00: ap_idle <= awdata[2];
-                endcase
             end
         end
     end
+
+    // Implement your AXI-Stream output logic here based on the filtered data
+    // stored in the output_data register.
+    
+    // Output data to AXI-Stream interface
+    always @(posedge axis_clk or negedge axis_rst_n) begin
+        if (!axis_rst_n) begin
+            // Reset AXI-Stream signals
+            sm_tvalid <= 0;
+            sm_tdata <= 0;
+            sm_tlast <= 0;
+        end else begin
+            // Output data to AXI-Stream
+            sm_tvalid <= (data_count < len_read_data);
+            sm_tdata <= output_data;
+            sm_tlast <= (data_count == len_read_data - 1);
+        end
+end
 
 endmodule
