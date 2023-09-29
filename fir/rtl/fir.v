1bcbf28e46ce77c19afeeff404e78dfd19b9c497
Doggoro <99811661+Chih-Ling-Wu@users.noreply.github.com>
2023-09-29 18:23:37 +0800

    Update fir.v

--- fir/rtl/fir.v
+++ fir/rtl/fir.v
@@ -24,53 +24,73 @@ module fir
     input   wire                     axis_rst_n
 );
     // Define internal signals and registers here
-    reg signed [(pDATA_WIDTH-1):0] shift_reg [0:Tape_Num-1];
+    reg signed [(pDATA_WIDTH-1):0] shift_reg [0:9]; // Size = 10 DW
     reg signed [(pDATA_WIDTH-1):0] output_data;
-    reg [pDATA_WIDTH-1:0] tap_coefficients [0:Tape_Num-1];
+    reg [10:0] tap_coefficients [0:10]; // Tap_RAM = 11 DW
     reg [pADDR_WIDTH-1:0] tap_data_addr;
     reg [pDATA_WIDTH-1:0] tap_data;
     reg [pDATA_WIDTH-1:0] len;
     reg ap_start;
+    reg ap_done;
+    reg ap_idle;
 
     // Implement your FIR filter logic here
+    // Using one Multiplier and one Adder
+
     always @(posedge axis_clk or negedge axis_rst_n) begin
         if (!axis_rst_n) begin
             // Reset logic here (initialize registers, etc.)
-            for (int i = 0; i < Tape_Num; i = i + 1) begin
+            for (int i = 0; i < 10; i = i + 1) begin
                 shift_reg[i] <= 0;
+            end
+            for (int i = 0; i < 11; i = i + 1) begin
                 tap_coefficients[i] <= 0;
             end
             len <= 0;
             ap_start <= 0;
+            ap_done <= 0;
+            ap_idle <= 0;
         end else begin
             // FIR filter logic here
 
             // Handle data input (axi_stream to shift register)
-            if (ap_start && awvalid && awready) begin
-                shift_reg[0] <= wdata;
-                for (int i = 1; i < Tape_Num; i = i + 1) begin
-                    shift_reg[i] <= shift_reg[i - 1];
+            if (!ap_start && awvalid && awready) begin
+                ap_start <= awdata[0];
+            end
+
+            if (ap_start) begin
+                if (awvalid && awready) begin
+                    shift_reg[0] <= wdata;
+                    for (int i = 1; i < 10; i = i + 1) begin
+                        shift_reg[i] <= shift_reg[i - 1];
+                    end
+                end
+
+                // Calculate FIR output using one multiplier and one adder
+                output_data <= 0;
+                for (int i = 0; i < 11; i = i + 1) begin
+                    output_data <= output_data + (shift_reg[i] * tap_coefficients[i]);
+                end
+
+                // Handle data output (axi_stream)
+                if (ss_tvalid && ss_tready) begin
+                    ss_tready <= 0;
+                    ss_tdata <= output_data;
+                    ss_tlast <= ss_tlast;
+                end
+
+                // Check for ap_done (when all data is processed)
+                if (ss_tvalid && ss_tlast) begin
+                    ap_done <= 1;
                 end
             end
 
-            // Calculate FIR output
-            output_data <= 0;
-            for (int i = 0; i < Tape_Num; i = i + 1) begin
-                output_data <= output_data + (shift_reg[i] * tap_coefficients[i]);
-            end
-
-            // Handle data output (axi_stream)
-            if (ap_start && ss_tvalid && ss_tready) begin
-                ss_tready <= 0;
-                ss_tdata <= output_data;
-                ss_tlast <= ss_tlast;
-            end
-
-            // Handle tap coefficient read (axilite)
+            // Handle axilite registers
             if (arvalid && arready) begin
                 case(araddr)
-                    12'h20: rdata <= tap_coefficients[araddr[4:0]];
+                    12'h20: rdata <= tap_coefficients[araddr[7:0]];
                     12'h10: rdata <= len;
+                    12'h00: rdata <= {ap_idle, ap_done, ap_start};
                     // Add more cases for other registers if needed
                     default: rdata <= 32'h0;
                 endcase
@@ -79,19 +99,14 @@ module fir
                 rvalid <= 0;
             end
 
-            // Handle tap coefficient write (axilite)
+            // Handle axilite writes
             if (ap_start && awvalid && awready) begin
                 case(awaddr)
-                    12'h20: tap_coefficients[awaddr[4:0]] <= wdata;
+                    12'h20: tap_coefficients[awaddr[7:0]] <= wdata;
                     12'h10: len <= wdata;
-                    // Add more cases for other registers if needed
+                    12'h00: ap_idle <= awdata[2];
                 endcase
             end
         end
     end
-
-    // Add more logic to handle other axilite registers and controls
-
-    // Add logic for generating awready, wready, arready, sm_tvalid, sm_tdata, sm_tlast
-
 endmodule
