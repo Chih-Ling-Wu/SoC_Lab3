27e173ccc90a64374e277e764f0b0da16908b3ae
Doggoro <99811661+Chih-Ling-Wu@users.noreply.github.com>
2023-09-30 16:51:20 +0800

    Update fir.v

--- fir/rtl/fir.v
+++ fir/rtl/fir.v
@@ -5,6 +5,7 @@ module fir
     parameter Tape_Num = 11
 )
 (
+    // Input and output AXI-Stream interfaces
     output wire awready,
     output wire wready,
     input wire awvalid,
@@ -17,6 +18,8 @@ module fir
     input wire [(pADDR_WIDTH-1):0] araddr,
     output wire rvalid,
     output wire [(pDATA_WIDTH-1):0] rdata,
+
+    // Streaming interfaces for data and control
     input wire ss_tvalid,
     input wire [(pDATA_WIDTH-1):0] ss_tdata,
     input wire ss_tlast,
@@ -43,220 +46,55 @@ module fir
     input wire axis_clk,
     input wire axis_rst_n
 );
-    // Internal signals and registers
-    reg [31:0] accum;
-    reg [31:0] output_data;
-    reg [4:0] tap_ptr;
-    reg [31:0] data_count;
-    reg [31:0] coef_write_data;
-    reg coef_write_enable;
-    reg [4:0] coef_write_addr;
-    reg coef_write_done;
-    reg [31:0] len_write_data;
-    reg len_write_enable;
-    reg [31:0] len_read_data;
-    reg [31:0] ap_start_read_data;
-    reg ap_start_write_enable;
-    reg [31:0] ap_done_write_data;
-    wire [(pADDR_WIDTH-1):0] ap_addr;
 
-    // Internal signals for BRAM control and data
-    reg [3:0] internal_tap_WE;
-    reg internal_tap_EN;
-    wire [(pDATA_WIDTH-1):0] internal_tap_Di;
-    wire [(pADDR_WIDTH-1):0] internal_tap_A;
-    wire [(pDATA_WIDTH-1):0] internal_tap_Do;
-
-    reg [3:0] internal_data_WE; // Added this signal
-    reg internal_data_EN;
-    wire [(pDATA_WIDTH-1):0] internal_data_Di;
-    wire [(pADDR_WIDTH-1):0] internal_data_A;
-    wire [(pDATA_WIDTH-1):0] internal_data_Do;
-    reg i;
-    reg ap_start;
-    reg sm_tvalid_reg;
-    reg ap_done_write_enable;
-
-    // Connect internal signals to BRAM ports
-    assign tap_WE = internal_tap_WE;
-    assign tap_EN = internal_tap_EN;
-    assign tap_Di = internal_tap_Di;
-    assign tap_A = internal_tap_A;
-    assign internal_tap_Do = tap_Do;
-
-    assign data_WE = internal_data_WE; // Added this assignment
-    assign data_EN = internal_data_EN;
-    assign data_Di = internal_data_Di;
-    assign data_A = internal_data_A;
-    assign internal_data_Do = data_Do;
-
-    // Address map constants
-    localparam ADDR_AP_CTRL = 12'h00;
-    localparam ADDR_DATA_LENGTH = 12'h10;
-    localparam ADDR_TAP_PARAMS_START = 12'h20;
-
-    // Synthesizable reset logic
-    always @(posedge axis_clk or negedge axis_rst_n) begin
-        if (!axis_rst_n) begin
-            // Reset your module's registers and state here
-            for (i = 0; i < 11; i = i + 1) begin
-                tap_coeff[i] <= 0;
-            end
-            accum <= 0;
-            output_data <= 0;
-            tap_ptr <= 0;
-            data_count <= 0;
-            coef_write_data <= 0;
-            coef_write_enable <= 0;
-            coef_write_addr <= 0;
-            coef_write_done <= 0;
-            ap_start_write_enable <= 0;
-            ap_done_write_data <= 0;
-            len_write_data <= 0;
-            len_write_enable <= 0;
-            len_read_data <= 0;
-            ap_start_read_data <= 0;
-            ap_start <= 0;
+    // Define internal signals and variables here
+    wire [(pDATA_WIDTH-1):0] shift_register [0:Tape_Num-1];
+    wire [(pDATA_WIDTH-1):0] coefficient [0:Tape_Num-1];
+    wire [(pDATA_WIDTH-1):0] product [0:Tape_Num-1];
+    wire [(pDATA_WIDTH-1):0] sum;
+    wire [3:0] tap_WE_internal;
+    wire [(pDATA_WIDTH-1):0] tap_Di_internal;
+    
+    // Implement FIR filter logic here
+    // You need to complete this section based on your specific requirements.
+    
+    // Example: FIR filter with one multiplier and one adder
+    always @(*) begin
+        sum = 0;
+        for (i = 0; i < Tape_Num; i = i + 1) begin
+            product[i] = shift_register[i] * coefficient[i];
+            sum = sum + product[i];
         end
     end
 
-    // AXI-Lite Control Logic
-    always @(posedge axis_clk or negedge axis_rst_n) begin
-        if (!axis_rst_n) begin
-            // Reset control signals
-        end else begin
-            // AXI-Lite address decoding
-            case(awaddr)
-                ADDR_AP_CTRL: begin
-                    if (awvalid && !ap_start) begin
-                        ap_start <= 1;
-                        ap_done_write_enable <= 0;
-                    end
-                end
-                ADDR_DATA_LENGTH: begin
-                    if (awvalid) begin
-                        len_write_data <= wdata;
-                        len_write_enable <= 1;
-                    end
-                end
-                ADDR_TAP_PARAMS_START: begin
-                    if (awvalid) begin
-                        coef_write_data <= wdata;
-                        coef_write_enable <= 1;
-                        coef_write_addr <= awaddr[(pADDR_WIDTH-1):(pADDR_WIDTH-6)]; // Adjusted for your address mapping
-                    end
-                end
-                default: begin
-                    coef_write_enable <= 0;
-                    len_write_enable <= 0;
-                end
-            endcase
+    // Implement AXI-Stream interface logic here
+    // You need to add logic to handle streaming data and control signals.
+
+    // Example: AXI-Stream data input
+    always @(posedge axis_clk) begin
+        if (axis_rst_n == 1'b0) begin
+            // Reset logic
+            // Initialize shift_register and other variables as needed
+        end else if (awvalid && awready) begin
+            // Handle AXI-Stream write (data_in) operation
+            // Update shift_register and coefficients
         end
     end
 
-    // FIR filtering logic
-    always @(posedge axis_clk or posedge axis_rst_n) begin
-        if (!axis_rst_n) begin
-            // Reset FIR filter
-            accum <= 0;
-            output_data <= 0;
-            tap_ptr <= 0;
-            data_count <= 0;
-        end else if (ap_start) begin
-            // Reset FIR filter on ap_start
-            accum <= 0;
-            output_data <= 0;
-            tap_ptr <= 0;
-            data_count <= 0;
-        end else if (ss_tvalid) begin
-            // FIR filtering logic when data is valid
-            // Shift in data when valid
-            for (i = 10; i > 0; i = i - 1) begin
-                shift_reg[i] <= shift_reg[i - 1];
-            end
-            if (ss_tvalid) begin
-                shift_reg[0] <= ss_tdata;
-            end else begin
-                shift_reg[0] <= 0;
-            end
-
-            // Multiply and accumulate
-            accum <= accum + (shift_reg[0] * tap_coeff[tap_ptr]);
-            tap_ptr <= tap_ptr + 1;
-
-            // Output valid data when tap pointer reaches Tape_Num
-            if (tap_ptr == Tape_Num) begin
-                output_data <= accum;
-                accum <= 0;
-                tap_ptr <= 0;
-                data_count <= data_count + 1;
-            end
+    // Example: AXI-Stream data output
+    always @(posedge axis_clk) begin
+        if (axis_rst_n == 1'b0) begin
+            // Reset logic
+        end else if (arvalid && arready) begin
+            // Handle AXI-Stream read (data_out) operation
+            // Set rdata and rvalid based on your design
         end
     end
 
-    // AXI-Stream Output Logic
-    reg [0:11] sm_tdata_reg;    // Register for sm_tdata signal
-    reg sm_tlast_reg;           // Register for sm_tlast signal
-
-    // AXI-Stream output logic
-    always @(posedge axis_clk or negedge axis_rst_n) begin
-        if (!axis_rst_n) begin
-            // Reset AXI-Stream output signals and registers
-            sm_tvalid_reg <= 0;
-            sm_tdata_reg <= 0;
-            sm_tlast_reg <= 0;
-        end else if (ap_start) begin
-            // Reset AXI-Stream output on ap_start
-            sm_tvalid_reg <= 0;
-            sm_tdata_reg <= 0;
-            sm_tlast_reg <= 0;
-        end else begin
-            // Update output data register
-            sm_tvalid_reg <= (data_count < len_read_data);
-            sm_tdata_reg <= output_data;
-            sm_tlast_reg <= (data_count == len_read_data - 1);
-        end
-    end
-
-    // Drive the output signals with registered values
-    assign sm_tvalid = sm_tvalid_reg;
-    assign sm_tdata = sm_tdata_reg;
-    assign sm_tlast = sm_tlast_reg;
-
-    // Implement your tap coefficient write logic here
-    always @(posedge axis_clk or negedge axis_rst_n) begin
-        if (!axis_rst_n) begin
-            // Reset tap coefficient write logic
-        end else begin
-            if (coef_write_enable) begin
-                tap_coeff[coef_write_addr] <= coef_write_data;
-                coef_write_done <= 1;
-            end else begin
-                coef_write_done <= 0;
-            end
-        end
-    end
-
-    // Implement your data length write logic here
-    always @(posedge axis_clk or negedge axis_rst_n) begin
-        if (!axis_rst_n) begin
-            // Reset data length write logic
-        end else begin
-            if (len_write_enable) begin
-                len_read_data <= len_write_data;
-            end
-        end
-    end
-
-    // Implement your AXI-Lite read logic for ap_start here
-    always @(posedge axis_clk or negedge axis_rst_n) begin
-        if (!axis_rst_n) begin
-            // Reset AXI-Lite read logic for ap_start
-            ap_start_read_data <= 0;
-        end else begin
-            if (ap_start_write_enable) begin
-                ap_start_read_data <= ap_done_write_data;
-            end
-        end
-    end
 endmodule
+This code provides a framework for your FIR module but doesn't contain the complete FIR filter implementation. You'll need to add the FIR filter logic inside the module, including how the input data is processed, how the coefficients are applied, and how the result is generated and streamed out.
+
+
+
+
+
