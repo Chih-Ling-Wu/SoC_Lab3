9bb11075b79f0f4dc70ffae27b79360940c9e45e
Doggoro <99811661+Chih-Ling-Wu@users.noreply.github.com>
2023-10-01 15:45:10 +0800

    Update fir.v

--- fir/rtl/fir.v
+++ fir/rtl/fir.v
@@ -1,100 +1,50 @@
-module fir
-#(
-    parameter pADDR_WIDTH = 12,
+module fir 
+#(  parameter pADDR_WIDTH = 12,
     parameter pDATA_WIDTH = 32,
-    parameter Tape_Num = 11
+    parameter Tape_Num    = 11
 )
 (
-    // Input and output AXI-Stream interfaces
-    output wire awready,
-    output wire wready,
-    input wire awvalid,
-    input wire [(pADDR_WIDTH-1):0] awaddr,
-    input wire wvalid,
-    input wire [(pDATA_WIDTH-1):0] wdata,
-    output wire arready,
-    input wire rready,
-    input wire arvalid,
-    input wire [(pADDR_WIDTH-1):0] araddr,
-    output wire rvalid,
-    output wire [(pDATA_WIDTH-1):0] rdata,
+    output  wire                     awready,
+    output  wire                     wready,
+    input   wire                     awvalid,
+    input   wire [(pADDR_WIDTH-1):0] awaddr,
+    input   wire                     wvalid,
+    input   wire [(pDATA_WIDTH-1):0] wdata,
+    output  wire                     arready,
+    input   wire                     rready,
+    input   wire                     arvalid,
+    input   wire [(pADDR_WIDTH-1):0] araddr,
+    output  wire                     rvalid,
+    output  wire [(pDATA_WIDTH-1):0] rdata,    
+    input   wire                     ss_tvalid, 
+    input   wire [(pDATA_WIDTH-1):0] ss_tdata, 
+    input   wire                     ss_tlast, 
+    output  wire                     ss_tready, 
+    input   wire                     sm_tready, 
+    output  wire                     sm_tvalid, 
+    output  wire [(pDATA_WIDTH-1):0] sm_tdata, 
+    output  wire                     sm_tlast, 
+    
+    // bram for tap RAM
+    output  wire [3:0]               tap_WE,
+    output  wire                     tap_EN,
+    output  wire [(pDATA_WIDTH-1):0] tap_Di,
+    output  wire [(pADDR_WIDTH-1):0] tap_A,
+    input   wire [(pDATA_WIDTH-1):0] tap_Do,
 
-    // Streaming interfaces for data and control
-    input wire ss_tvalid,
-    input wire [(pDATA_WIDTH-1):0] ss_tdata,
-    input wire ss_tlast,
-    output wire ss_tready,
-    input wire sm_tready,
-    output wire sm_tvalid,
-    output wire [(pDATA_WIDTH-1):0] sm_tdata,
-    output wire sm_tlast,
+    // bram for data RAM
+    output  wire [3:0]               data_WE,
+    output  wire                     data_EN,
+    output  wire [(pDATA_WIDTH-1):0] data_Di,
+    output  wire [(pADDR_WIDTH-1):0] data_A,
+    input   wire [(pDATA_WIDTH-1):0] data_Do,
 
-    // BRAM for tap RAM
-    output wire [3:0] tap_WE,
-    output wire tap_EN,
-    output wire [(pDATA_WIDTH-1):0] tap_Di,
-    output wire [(pADDR_WIDTH-1):0] tap_A,
-    input wire [(pDATA_WIDTH-1):0] tap_Do,
-
-    // BRAM for data RAM
-    output wire [3:0] data_WE,
-    output wire data_EN,
-    output wire [(pDATA_WIDTH-1):0] data_Di,
-    output wire [(pADDR_WIDTH-1):0] data_A,
-    input wire [(pDATA_WIDTH-1):0] data_Do,
-
-    input wire axis_clk,
-    input wire axis_rst_n
+    input   wire                     axis_clk,
+    input   wire                     axis_rst_n
 );
+begin
 
-    // Define internal signals and variables here
-    wire [(pDATA_WIDTH-1):0] shift_register [0:Tape_Num-1];
-    wire [(pDATA_WIDTH-1):0] coefficient [0:Tape_Num-1];
-    wire [(pDATA_WIDTH-1):0] product [0:Tape_Num-1];
-    wire [(pDATA_WIDTH-1):0] sum;
-    wire [3:0] tap_WE_internal;
-    wire [(pDATA_WIDTH-1):0] tap_Di_internal;
-    
-    // Implement FIR filter logic here
-    // You need to complete this section based on your specific requirements.
-    
-    // Example: FIR filter with one multiplier and one adder
-    always @(*) begin
-        sum = 0;
-        for (i = 0; i < Tape_Num; i = i + 1) begin
-            product[i] = shift_register[i] * coefficient[i];
-            sum = sum + product[i];
-        end
-    end
-
-    // Implement AXI-Stream interface logic here
-    // You need to add logic to handle streaming data and control signals.
-
-    // Example: AXI-Stream data input
-    always @(posedge axis_clk) begin
-        if (axis_rst_n == 1'b0) begin
-            // Reset logic
-            // Initialize shift_register and other variables as needed
-        end else if (awvalid && awready) begin
-            // Handle AXI-Stream write (data_in) operation
-            // Update shift_register and coefficients
-        end
-    end
-
-    // Example: AXI-Stream data output
-    always @(posedge axis_clk) begin
-        if (axis_rst_n == 1'b0) begin
-            // Reset logic
-        end else if (arvalid && arready) begin
-            // Handle AXI-Stream read (data_out) operation
-            // Set rdata and rvalid based on your design
-        end
-    end
-
-endmodule
-This code provides a framework for your FIR module but doesn't contain the complete FIR filter implementation. You'll need to add the FIR filter logic inside the module, including how the input data is processed, how the coefficients are applied, and how the result is generated and streamed out.
+    // write your code here!
 
 
-
-
-
+end
