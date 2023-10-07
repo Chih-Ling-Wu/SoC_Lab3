80ac75a330c86f6dda12348461e93d0ad4385b97
Doggoro <99811661+Chih-Ling-Wu@users.noreply.github.com>
2023-10-07 21:29:10 +0800

    Update bram11.v

--- bram/bram11.v
+++ bram/bram11.v
@@ -14,31 +14,28 @@ module bram11
     input   wire    [3:0]   WE;
     input   wire            EN;
     input   wire    [31:0]  Di;
-    output  wire     [31:0]  Do;
+    output  reg     [31:0]  Do;
     input   wire    [11:0]   A; 
 
     //  11 words
-	reg [31:0] RAM[0:11];
+	reg [31:0] RAM[0:10];
     reg [11:0] r_A;
 
     always @(posedge CLK) begin
         r_A <= A;
     end
 
-
-    assign Do = {32{EN}} & RAM[r_A-'d32]; // read
-
-
+    // assign Do = {32{EN}} & RAM[r_A>>2];    // read
+    always@* begin
+        Do = {32{EN}} & RAM[r_A>>2];
+    end
     reg [31:0] Temp_D;
     always @(posedge CLK) begin
         if(EN) begin
-            Temp_D = RAM[A-'d32];
-            if(WE == 4'b1111) RAM[A-'d32] <= Di;
-
-            // if(WE[0]) RAM[A>>2] <= {Temp_D[31:24], Temp_D[23:16], Temp_D[15:8], Di[7:0]};
-            // if(WE[1]) RAM[A>>2] <= {Temp_D[31:24], Temp_D[23:16], Di[15:8], Temp_D[7:0]};
-            // if(WE[2]) RAM[A>>2] <= {Temp_D[31:24], Di[23:16], Temp_D[15:8], Temp_D[7:0]};
-            // if(WE[3]) RAM[A>>2] <= {Di[31:24], Temp_D[23:16], Temp_D[15:8], Temp_D[7:0]};
+	    if(WE[0]) RAM[A>>2][7:0] <= Di[7:0];
+            if(WE[1]) RAM[A>>2][15:8] <= Di[15:8];
+            if(WE[2]) RAM[A>>2][23:16] <= Di[23:16];
+            if(WE[3]) RAM[A>>2][31:24] <= Di[31:24];
         end
     end
 
