2087b015d0880e355def2f172ad95e25509e1d39
Doggoro <99811661+Chih-Ling-Wu@users.noreply.github.com>
2023-10-12 13:38:10 +0800

    Update fir.v

--- fir/rtl/fir.v
+++ fir/rtl/fir.v
@@ -1,4 +1,25 @@
 `timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2023 13:38:55 PM
+// Design Name: Chih Ling Wu
+// Module Name: fir
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module fir 
 #(  parameter pADDR_WIDTH = 12,
     parameter pDATA_WIDTH = 32,
